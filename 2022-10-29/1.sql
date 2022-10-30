fcc96e40a968e55252e71d6899f52f9f49d45251
xixi <2990698929@qq.com>
2022-10-30 20:54:01 +0800

    finish

--- 2022-10-29/1.sql
+++ 2022-10-29/1.sql
@@ -1,26 +1,28 @@
 use StuDB
 go
 
--- 任务一
----- 添加表字段
---alter table dbo.教师信息 add phone_number char(11);
----- 添加表字段并为字段添加约束
---alter table dbo.教师信息 add annual_salary decimal(7,2) check (annual_salary<1000000.00);
----- 修改表字段
---alter table dbo.教师信息 alter column 性别 bit;
----- 约束字段不得为空
---alter table dbo.教师信息 alter column 姓名 nchar(5) not null;
------- 设置字段为主键
---alter table dbo.课程信息 add constraint 课程号 primary key(课程号);、
---alter table dbo.学生信息 add  constraint 学号 primary key(学号);
---alter table dbo.学生成绩 add constraint 学号课程号 primary key(学号,课程号);
+ --任务一
+-- 添加表字段
+alter table dbo.教师信息 add phone_number char(11);
+-- 添加表字段并为字段添加约束
+alter table dbo.教师信息 add annual_salary decimal(7,2) check (annual_salary<1000000.00);
+-- 修改表字段
+alter table dbo.教师信息 alter column 性别 bit;
+-- 约束字段不得为空
+alter table dbo.教师信息 alter column 姓名 nchar(5) not null;
+---- 设置字段为主键
+alter table dbo.课程信息 add constraint 课程号 primary key(课程号);、
+alter table dbo.学生信息 add  constraint 学号 primary key(学号);
+alter table dbo.学生成绩 add constraint 学号课程号 primary key(学号,课程号);
 
 
---alter table dbo.课程信息 add constraint 课程名 unique(课程名);
+alter table dbo.课程信息 add constraint 课程名 unique(课程名);
 
--- 该做任务一7.  了
-
--- 任务二
-
-
+ --给表字段添加默认值
+--给某个字段设置约束并设置默认值的时候，需要先将原先的约束删除，然后再新建约束设置默认值，否则报错。
+alter table dbo.学生信息 add constraint 专业_ default '网络工程' for 专业;
+alter table dbo.学生信息 add constraint 系部_ default '信创学院' for 系部;
+alter table dbo.课程信息 add constraint range check(开课学期 <=6 and 开课学期 >=0);
+alter table dbo.学生成绩 add constraint FK_学号 foreign key(学号) references dbo.学生信息(学号);
+alter table dbo.学生成绩 add constraint FK_课程号 foreign key(课程号) references dbo.课程信息(课程号);
 go
---
+++ 2022-10-29/3.sql
@@ -0,0 +1,19 @@
+﻿-- 任务五  修改表中数据
+-- 4.
+update dbo.学生信息 set 出生日期 = '19970326' where 姓名 = '杨勇';
+go
+
+update dbo.学生信息 set 备注 = '班长' where 姓名 = '陈圆';
+go
+
+-- 5.
+update dbo.课程信息 set 课程名 = 'C语言程序设计' where 课程名='程序设计基础';
+
+-- 6.
+update dbo.课程信息 set 学分 = 5 where 学分 = 3;
+
+-- 任务6 -- 7.
+-- 删除表中数据
+-- 按照行号删除数据 -- 🤣算了算了，都没行号
+delete from dbo.学生成绩 where 成绩 =67 or 成绩 = 57;
+
